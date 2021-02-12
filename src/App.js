feb9e32e92037f74e0a89d5d6a41ad9db1561ee9
yogesh-karki <yogesh@hueshine.com>
2021-02-12 15:19:31 +0545

    complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Navbar from './components/Navbar'
-import './App.css'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import './App.css';
+import Header from './components/Header';
+import Particles from "react-particles-js";
+
+
 
 const App = () => {
     return ( 
-        <div>
+        <>
+            <Particles  
+            className="particles-canvas"
+                params= {{
+                    particles: {
+                        number : {
+                            value: 30,
+                            density: true, 
+                            value_area: 250000
+                        }
+                    },
+                    shape: {
+                        type: 'circle',
+                        stroke : {
+                            width: 6,
+                            color: "#f9ab00"
+                        }
+                    }
+                }}
+            />
             <Navbar />
-        </div>
+            <Header />
+        </>
      );
 }
  
